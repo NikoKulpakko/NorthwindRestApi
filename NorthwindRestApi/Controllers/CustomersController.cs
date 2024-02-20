4c150d2180631b34858c79948a02773ee82ca17b
Venomace93 <niko.kulpakko@gmail.com>
2024-02-20 12:50:51 +0200

    lisätty juttuja

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -13,10 +13,56 @@ namespace NorthwindRestApi.Controllers
 
         //Metodi joka hakee kaikki asiakkaat
         [HttpGet]
-        public ActionResult GetAllCustomers() 
+        public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää:" + ex.InnerException);            
+            }
+        }
+
+        //Metodi joka hakee asiakkaan
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+
+                var asiakas = db.Customers.Find();
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    return BadRequest($"Asiakasta id:llä  {id}  ei löydy");
+                }
+            }
+            catch (Exception e) 
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää:" + e);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust)
+        {
+            try 
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok("Lisättiin uusi asiakas" + cust.CompanyName);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää:" + e.InnerException);
+            }
         }
     }
 }
